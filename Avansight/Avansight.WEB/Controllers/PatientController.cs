3b1377dc5e45de876e8ef9cec1d1b088d912ae46
ZhidongChang <westsavage@gmail.com>
2021-06-25 11:07:28 -0700

    UI added

--- Avansight/Avansight.WEB/Controllers/PatientController.cs
+++ Avansight/Avansight.WEB/Controllers/PatientController.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Avansight.WEB.Controllers
 {
+    public class GeneratePatients
+    {
+        public float size { get; set; }
+        public float male { get; set; }
+        public float female { get; set; }
+        public float age20 { get; set; }
+        public float age30 { get; set; }
+        public float age40 { get; set; }
+        public float age50 { get; set; }
+        public float age60 { get; set; }
+    }
     public class PatientController : Controller
     {
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public JsonResult JsonGeneratePatients(string data)
+        {
+            GeneratePatients generatePatients = JsonConvert.DeserializeObject<GeneratePatients>(data);
+            float generateMaleNumbers =( generatePatients.male / (generatePatients.male + generatePatients.female) ) * generatePatients.size;
+            float generateFemaleNumbers = generatePatients.size - generateMaleNumbers;
+            float totalAgeNumbers = generatePatients.age20 + generatePatients.age30 + generatePatients.age40 + generatePatients.age50 + generatePatients.age60;
+            float generateAge20Numbers = (generatePatients.age20 / totalAgeNumbers) * generatePatients.size;
+            float generateAge30Numbers = (generatePatients.age30 / totalAgeNumbers) * generatePatients.size;
+            float generateAge40Numbers = (generatePatients.age40 / totalAgeNumbers) * generatePatients.size;
+            float generateAge50Numbers = (generatePatients.age50 / totalAgeNumbers) * generatePatients.size;
+            float generateAge60Numbers = (generatePatients.age60 / totalAgeNumbers) * generatePatients.size;
+
+            return Json(data);
         }
     }
 }
--- Avansight/Avansight.WEB/Startup.cs
+++ Avansight/Avansight.WEB/Startup.cs
@@ -15,19 +15,17 @@ namespace Avansight.WEB
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseMvc(routes =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Patient}/{action=Index}/{id?}");
             });
         }
     }
